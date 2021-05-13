1675e9a733c6153f5da4881d77bf1d4ecdde5337
Eduardo Sayans <edusayans@gmail.com>
2021-05-13 18:27:21 -0300

    Aula 02 OK

---
+++ src/entities/Settings.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn} from "typeorm"
+
+import { v4 as uuid } from "uuid"
+
+@Entity("settigns")
+class Setting {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    chat: boolean;
+
+    @UpdateDateColumn()
+    updated_at: string;
+
+    @CreateDateColumn()
+    created_at: string;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Setting } 
